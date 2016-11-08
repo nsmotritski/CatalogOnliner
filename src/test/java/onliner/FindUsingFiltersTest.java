48daa32224bcf28173d0264b0f24429039543b1b
nsmotritski <nsmotritski@gmail.com>
2016-11-08 22:11:28 +0300

    Selenium version updated to 2.53, latest version of the chromedriver.exe is added, but sill fails with "An error occurred while instantiating class" error even for demo test

--- src/test/java/onliner/FindUsingFiltersTest.java
+++ src/test/java/onliner/FindUsingFiltersTest.java
@@ -3,12 +3,19 @@ package onliner;
 import onliner.forms.CatalogOnlinerPage;
 import onliner.forms.CatalogOnlinerTVs;
 import onliner.forms.OnlinerHomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import webdriver.BaseTest;
+import webdriver.Browser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindUsingFiltersTest extends BaseTest {
     public void runTest() {
 
         logger.step(1);
+        Browser.getInstance();
         OnlinerHomePage onlinerHomePage = new OnlinerHomePage();
         onlinerHomePage.assertLinkPresent();
 
@@ -28,5 +35,7 @@ public class FindUsingFiltersTest extends BaseTest {
         catalogOnlinerTVs.setDiagonalToFilter("42");
 
         logger.step(4);
+        List<WebElement> searchItemList = new ArrayList<WebElement>();
+        searchItemList = browser.getDriver().findElements(By.xpath(".//div[@id='schema-products']"));
     }
 }
--- src/test/java/onliner/forms/CatalogOnlinerTVs.java
+++ src/test/java/onliner/forms/CatalogOnlinerTVs.java
@@ -14,7 +14,7 @@ public class CatalogOnlinerTVs extends BaseForm {
     private Checkbox manufacturerFilter = new Checkbox(By.xpath("//span[@class='schema-filter__checkbox-text' and .='Samsung']"));
     private TextBox maxPriceFilter = new TextBox(By.xpath(".//*[@id='schema-filter']//input[@placeholder='до']"));
     private TextBox releasedAfterYearFilter = new TextBox(By.xpath(".//*[@id='schema-filter']//input[@placeholder='2011']"));
-    private Dropdown diagonalFromFilter = new Dropdown(By.xpath("//span[.='Диагональ']/../following-sibling::div/div/div/select[contains(@data-bind,\"from\")]"));
+    private Dropdown diagonalFromFilter = new Dropdown(By.xpath("//span[.='Диагональ']/../following-sibling::div/div/div/select[contains(@data-bind,'from')]"));
     private Dropdown diagonalToFilter = new Dropdown(By.xpath("//span[.='Диагональ']/../following-sibling::div/div/div/select[contains(@data-bind,\"to\")]"));
     private List<WebElement> searchResults;
 
