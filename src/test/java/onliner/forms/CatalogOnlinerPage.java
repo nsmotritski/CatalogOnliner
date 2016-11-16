8edc5ce99d844a3b4413ec18f2cb3bad37867a82
nsmotritski <nsmotritski@gmail.com>
2016-11-16 21:55:47 +0300

    Couple of locators added for the TVDetailPage

--- src/test/java/onliner/forms/CatalogOnlinerPage.java
+++ src/test/java/onliner/forms/CatalogOnlinerPage.java
@@ -8,15 +8,10 @@ import webdriver.elements.Link;
 
 public class CatalogOnlinerPage extends BaseForm {
     private Link menuItemLink = new Link(By.xpath("//div[@class='catalog-bar']/ul/li/a[.='Телевизоры']"),"MenuItemLink");
-    //div[@class='catalog-bar']/ul/li/a[.='Телевизоры']
-    //ul/li/span /descendant::a[.='Телевизоры']
-    private Label lbLogo = new Label(By.xpath(".//img[@alt='Onliner']"),"catalog onliner logo");
 
     public CatalogOnlinerPage() {
         super(By.xpath("//div[@id='fast-search']/form/input[@data-project='catalog_public']"), "Catalog Onliner.by");
     }
-
-    public void assertLogo(){assert(lbLogo.isPresent());}
 
     public void navigateMenuItem () {
         menuItemLink.click();
--- src/test/java/onliner/forms/TVDetailPage.java
+++ src/test/java/onliner/forms/TVDetailPage.java
@@ -3,26 +3,13 @@ package onliner.forms;
 import org.openqa.selenium.By;
 import webdriver.BaseForm;
 import webdriver.elements.Label;
-import webdriver.elements.Link;
 
-/**
- * Created by Николай on 16.11.2016.
- */
 public class TVDetailPage extends BaseForm {
+    private Label tvReleaseYear = new Label(By.xpath(".//td[contains(.,'Дата')] /following-sibling::td/span"),"tvReleaseYear");
+    private Label tvDiagonal = new Label(By.xpath(".//td[contains(.,'Диагональ')] /following-sibling::td/span"),"tvDiagonal");
 
-        private Link menuItemLink = new Link(By.xpath("//div[@class='catalog-bar']/ul/li/a[.='Телевизоры']"),"MenuItemLink");
-        //div[@class='catalog-bar']/ul/li/a[.='Телевизоры']
-        //ul/li/span /descendant::a[.='Телевизоры']
-        private Label lbLogo = new Label(By.xpath(".//img[@alt='Onliner']"),"catalog onliner logo");
-
-        public TVDetailPage() {
-            super(By.xpath("//div[@id='fast-search']/form/input[@data-project='catalog_public']"), "Catalog Onliner.by");
-        }
-
-        public void assertLogo(){assert(lbLogo.isPresent());}
-
-        public void navigateMenuItem () {
-            menuItemLink.click();
-        }
+    public TVDetailPage() {
+        super(By.xpath("//div[@id='fast-search']/form/input[@data-project='catalog_public']"), "Catalog Onliner.by");
+    }
 
 }
